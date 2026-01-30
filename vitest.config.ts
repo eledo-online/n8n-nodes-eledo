da1a905ebba4be24cfb106077df0ca372f55f816
Vladimír Záhradník <4460766+vzahradnik@users.noreply.github.com>
2026-01-30 18:24:57 +0100

    chore: add vitest as a unit test dependency

---
+++ vitest.config.ts
@@ -0,0 +1,12 @@
+/// <reference types="vitest" />
+
+/* eslint-disable */
+import { defineConfig } from 'vitest/config';
+
+export default defineConfig({
+	test: {
+		environment: 'node',
+		globals: true,
+		include: ['tests/**/*.test.ts'],
+	},
+});
